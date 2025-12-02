bf2da1f5e9cc661f04919cef580d14e0d57d2a7c
iliasbgh <sv7h2vjsxx@privaterelay.appleid.com>
2025-12-02 10:30:22 +0100

    Update __init__.py
    page contact

--- __init__.py
+++ __init__.py
@@ -11,5 +11,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #ilias
   
+  
 if __name__ == "__main__":
   app.run(debug=True)
+  
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>
