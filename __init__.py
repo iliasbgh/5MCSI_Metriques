dc3683691b42fa6b6b09793de2d00395265d64e8
iliasbgh <sv7h2vjsxx@privaterelay.appleid.com>
2025-12-02 10:52:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,10 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)      
-
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
