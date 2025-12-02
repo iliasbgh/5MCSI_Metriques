3673a59ac49e3ea34af381cada5eee635fadf951
iliasbgh <sv7h2vjsxx@privaterelay.appleid.com>
2025-12-02 10:35:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,15 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #ilias
 
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #ilias
+
   
 if __name__ == "__main__":
   app.run(debug=True)
